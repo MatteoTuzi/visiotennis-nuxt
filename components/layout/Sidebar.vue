87df16b38286534a10053bd7fb6dc9f7dfba250b
MatteoTuzi <37384119+MatteoTuzi@users.noreply.github.com>
2023-09-26 11:37:18 +0200

    fix build

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -9,7 +9,7 @@
             class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-900 lg:translate-x-0 lg:static lg:inset-0">
             <div class="flex items-center justify-center mt-8">
                 <div class="flex items-center">
-                    <img src="img/VisioTennis_Logo.png" alt="logo" class="w-16 h-16" />
+                    <img src="/img/VisioTennis_Logo.png" alt="logo" class="w-16 h-16" />
 
                     <span class="mx-2 text-2xl font-semibold text-white">Visiotennis</span>
                 </div>
